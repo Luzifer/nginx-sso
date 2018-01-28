2d1f1f88553e262603976b8a87ef368d2f8c6216
Knut Ahlers <knut@ahlers.me>
2018-01-28 11:46:13 +0100

    Prevent double-login
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- main.go
+++ main.go
@@ -125,6 +125,12 @@ func handleAuthRequest(res http.ResponseWriter, r *http.Request) {
 }
 
 func handleLoginRequest(res http.ResponseWriter, r *http.Request) {
+	if _, _, err := detectUser(r); err == nil {
+		// There is already a valid user
+		http.Redirect(res, r, r.URL.Query().Get("go"), http.StatusFound)
+		return
+	}
+
 	if r.Method == "POST" {
 		err := loginUser(res, r)
 		switch err {
