f9118beb79aaad96950664a60b1860c1e7acc9ee
Knut Ahlers <knut@ahlers.me>
2020-06-30 00:35:34 +0200

    Remove vendored libraries
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- vendor/github.com/jda/go-crowd/error.go
+++
@@ -1,9 +0,0 @@
-package crowd
-
-// Error represents a error response from Crowd.
-// Error reasons are documented at https://developer.atlassian.com/display/CROWDDEV/Using+the+Crowd+REST+APIs#UsingtheCrowdRESTAPIs-HTTPResponseCodesandErrorResponses
-type Error struct {
-	XMLName struct{} `xml:"error"`
-	Reason  string   `xml:"reason"`
-	Message string   `xml:"message"`
-}
